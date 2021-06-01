5404f32730b16f0e47a45bf9b9a8158b5e0d5d5f
Mihadul Islam <mehad65@gmail.com>
2021-06-01 17:06:45 +0600

    A.Cards

---
+++ Codeforces/A. Cards/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+
+namespace A._Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            
+            StringBuilder str = new StringBuilder();
+            for (int i = 0; i < a; i++)
+            {
+                str.Append(Console.ReadKey().KeyChar);
+            }
+            Console.WriteLine();
+            int zcount = 0;
+            int ncount = 0;
+            foreach(var c in str.ToString())
+            {
+                if (c == 'z')
+                {
+                    zcount++;
+                }else if (c == 'n')
+                {
+                    ncount++;
+                }
+            }
+            for (int i = 0; i < ncount; i++)
+            {
+                Console.Write("1 ");
+            }
+            for (int i = 0; i < zcount; i++)
+            {
+                Console.Write("0 ");
+            }
+            
+        }
+    }
+}
