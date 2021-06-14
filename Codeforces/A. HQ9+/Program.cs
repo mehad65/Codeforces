14a1b0a46d001b7c113642cd4d8d58f9d2b39327
Mihadul Islam <mehad65@gmail.com>
2021-06-14 14:57:31 +0600

    ok

---
+++ Codeforces/A. HQ9+/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace A._HQ9_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            if (input.Contains("H") || input.Contains("Q") || input.Contains("9"))
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
---
+++ Codeforces/A. Nearly Lucky Number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace A._Nearly_Lucky_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int a = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i]=='4' || input[i] == '7')
+                {
+                    a++;
+                }
+            }
+            if (a==4 || a==7)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
