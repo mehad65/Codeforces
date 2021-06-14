ff49deba6c39be2dc63885295fd7ddbc9a512a0d
Mihadul Islam <mehad65@gmail.com>
2021-06-14 09:48:57 +0600

    last

---
+++ Codeforces/A. Word/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace A._Word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int a = 0;
+            int b = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i]>='a' && input[i] <= 'z')
+                {
+                    a++;
+                }
+                else
+                {
+                    b++;
+                }
+            }
+            if (a >= b)
+            {
+                Console.WriteLine(input.ToLower());
+            }
+            else
+            {
+                Console.WriteLine(input.ToUpper());
+            }
+        }
+    }
+}
---
+++ Codeforces/A. Young Physicist/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace A._Young_Physicist
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+            string[] arr = new string[a];
+            string[] arrtest = new string[3];
+            int b = 0;
+            int c = 0;
+            int d = 0;
+            for (int i = 0; i < a; i++)
+            {
+                arr[i] = Console.ReadLine();
+                
+                arrtest = arr[i].Split(" ");
+                b = b + Convert.ToInt32(arrtest[0]);
+                c = c + Convert.ToInt32(arrtest[1]);
+                d = d + Convert.ToInt32(arrtest[2]);
+
+            }
+            if (b==0 && c==0 && d==0)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+    }
+}
